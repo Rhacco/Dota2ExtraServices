3a8255dca9c0b613b5054375a74b65696d36def0
Nico Zimmermann <nicotom.zim@gmail.com>
2017-12-03 10:19:39 +0100

    Fixed position of check for corrupt API call

--- top_recent_matches.py
+++ top_recent_matches.py
@@ -12,7 +12,6 @@ __expiration_dates = {}  # compensate for the delay of the Steam Web API
 __fail_counters = {}  # compensate for corrupt API calls occurring sometimes
 
 def fetch_finished_matches():
-    to_remove = []
     for live_match in top_live_matches.data:
         match_id = int(live_match['match_id'])
         if match_id not in __finished_matches:
@@ -27,22 +26,13 @@ def fetch_finished_matches():
                 __expiration_dates[match_id] = expiration_date
             except Exception as e:
                 if str(e) != '\'Match ID not found\'':
-                    log('Failed to fetch finished match %s: %s' %
+                    log('Failed to check if match %s is finished: %s' %
                             (str(match_id), str(e)))
-                    if match_id not in __fail_counters:
-                        __fail_counters[match_id] = 0
-                    __fail_counters[match_id] += 1
-                    if __fail_counters[match_id] >= 5:
-                        log('Removing %s on %s, failed too often' %
-                                (str(match_id), str(live_match['server_id'])))
-                        to_remove.append(match_id)
-    for match_id in to_remove:
-        top_live_matches.remove(match_id)
-        __fail_counters.pop(match_id)
 
 def handle_finished_matches():
     now = datetime.datetime.now()
-    to_remove = []
+    to_remove_finished = []
+    to_remove_failed = []
     for match_id, expiration_date in __expiration_dates.items():
         if now > expiration_date:
             live_match = __finished_matches[match_id][0]
@@ -51,17 +41,29 @@ def handle_finished_matches():
                 data[match_id] = __convert(live_match, steam_finished_match)
                 data.move_to_end(match_id, last=False)  # needs Python 3.2+!
                 top_live_matches.remove(match_id)
-                to_remove.append(match_id)
+                to_remove_finished.append(match_id)
             except Exception as e:
                 log('Failed to handle finished match %s: %s' %
                         (str(match_id), str(e)))
-    for match_id in to_remove:
+                if match_id not in __fail_counters:
+                    __fail_counters[match_id] = 0
+                __fail_counters[match_id] += 1
+                if __fail_counters[match_id] >= 5:
+                    log('Removing %s on %s, failed too often' %
+                            (str(match_id), str(live_match['server_id'])))
+                    to_remove_failed.append(match_id)
+    for match_id in to_remove_finished:
         __finished_matches.pop(match_id)
         __expiration_dates.pop(match_id)
         if match_id in __fail_counters:
             __fail_counters.pop(match_id)
         if match_id in top_live_matches.fail_counters:
             top_live_matches.fail_counters.pop(match_id)
+    for match_id in to_remove_failed:
+        top_live_matches.remove(match_id)
+        __fail_counters.pop(match_id)
+        __finished_matches.pop(match_id)
+        __expiration_dates.pop(match_id)
     while len(data) > 20:  # only keep most recent top matches
         data.popitem()
 
